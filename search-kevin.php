1a02db20f7118708cb120c0effae5c042586a21f
killazilla54 <killazilla@Kevins-MacBook-Pro.local>
2015-03-20 23:42:40 -0400

    Basically done
    Basically done.  Will need to be edited once we have common.php
    complete.

--- search-kevin.php
+++ search-kevin.php
@@ -3,7 +3,11 @@
 <head>
 <meta charset="UTF-8">
 <title>Search Kevin Bacon</title>
-<!-- Links to provided files.  Do not edit or remove these links -->
+		<!-- php file for searching for movies where the entered actor and Kevin Bacon appear together
+        	@author: Kevin O'Neil
+        -->
+
+		<!-- Links to provided files.  Do not edit or remove these links -->
 		<link href="https://webster.cs.washington.edu/images/kevinbacon/favicon.png" type="image/png" rel="shortcut icon" />
 		<script src="https://webster.cs.washington.edu/js/kevinbacon/provided.js" type="text/javascript"></script>
 
@@ -36,26 +40,36 @@
 					if (!$link) {
     					die('Could not connect: ' . mysql_error());
 					}
-
+					//specify the database
 					$db = mysql_select_db('imdb', $link);
+					//Gets the searched actor's id
 					$sql = "SELECT id FROM actors WHERE first_name = '".$_GET['firstname']."' AND last_name = '".$_GET['lastname']."'";
+					//Gets Kevin Bacon's id
+					$baconSql = "SELECT id FROM actors WHERE first_name = 'Kevin' AND last_name = 'Bacon'";
 					
 					$result = mysql_query($sql, $link);
-					
 					$id = mysql_result($result,0, 'id');
 					
+					$result = mysql_query($baconSql, $link);
+					$baconId = mysql_result($result,0, 'id');
 					
+					/*	
+						Joins actors, movies, and roles tables, then Selects movie name and year 
+						for every movie that Kevin Bacon and the searched actor appear together in.
+					*/
 					$sql2 = "SELECT m.name, m.year ";
 					$sql2.= "FROM movies m ";
 					$sql2.= "JOIN roles r1 ON r1.movie_id = m.id ";
 					$sql2.= "JOIN actors a1 ON r1.actor_id = a1.id ";
 					$sql2.= "JOIN roles r2 ON r2.movie_id = m.id ";
 					$sql2.= "JOIN actors a2 ON r2.actor_id = a2.id ";
-					$sql2.= "WHERE ((r1.actor_id='22591') AND (r2.actor_id='".$id."') AND (r1.movie_id = r2.movie_id))";
+					$sql2.= "WHERE ((r1.actor_id='".$baconId."') AND (r2.actor_id='".$id."') AND (r1.movie_id = r2.movie_id))";
 					$sql2.= "ORDER BY m.year DESC";
 					$result = mysql_query($sql2, $link);
+					
 					$num=mysql_numrows($result);
 					$i = 0;
+					//Prints the results into the table
 					while($i < $num){								
 						echo "<tr><td>";
 						echo $i+1;
