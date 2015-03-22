f0f5a4beca026be568ec434ea52208e297bf2628
jdp93 <jdp93@uga.edu>
2015-03-22 14:27:38 -0400

    Fixed problems
    Will not confuse actors whose first names have similar initial characters

--- common.php
+++ common.php
@@ -1,29 +1,28 @@
 <?php
-    $link = mysql_connect('localhost', 'root', 'kaiden90');
-    if (!$link) {
-        die('Could not connect: ' . mysql_error());
-    }
-    //specify the database
-    $db = mysql_select_db('imdb', $link);
-    //Gets the searched actor's id
-    $sql = "SELECT id
+    $user = 'root';
+    $pass = '';
+    $dbh = new PDO('mysql:host=localhost;dbname=imdb', $user, $pass);
+
+//Gets the searched actor's id
+$q1 = "SELECT id
             FROM actors
-            WHERE first_name LIKE'".$_GET['firstname']."%' AND last_name = '".$_GET['lastname']."'
-            AND film_count >=
+            WHERE (first_name LIKE '".$_GET['firstname']." %' OR first_name = '".$_GET['firstname'].
+                    "') AND last_name = '".$_GET['lastname']."'
+                AND film_count >=
                 all(SELECT film_count
                     FROM actors
-                    WHERE first_name LIKE '".$_GET['firstname']."%' AND last_name = '".$_GET['lastname']."')";
-    $result = mysql_query($sql, $link);
-    $num=mysql_numrows($result);
-    if($num == 0){
-        echo "Actor ";
-        echo $_GET['firstname'];
-        echo " ";
-        echo $_GET['lastname'];
-        echo " not found.";
-    }
-    else {
-        $id = mysql_result($result, 0, 'id');
-    }
-    mysql_close($link);
+                    WHERE (first_name LIKE'".$_GET['firstname']." %' OR first_name = '".$_GET['firstname'].
+                    "') AND last_name = '".$_GET['lastname']."')";
+$id = null;
+foreach($dbh->query($q1) as $row){
+    $id = $row['id']	;
+}
+if($id == null){
+    echo "Actor ";
+    echo $_GET['firstname'];
+    echo " ";
+    echo $_GET['lastname'];
+    echo " not found.";
+}
+$dbh = null;
 ?>
